62b91d611463102894a278a6b73600c55ab80ffa
Mihail Mateev <mihail_mateev@epam.com>
2025-07-08 12:58:42 +0300

    Initial commit with static website

---
+++ streaming.php
@@ -0,0 +1,332 @@
+<?php
+// require_once("shared/sessionUtil.php");
+// sec_session_start(FALSE);
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Streams | js.talks("Bulgaria"); </title>
+  <meta name="description" content="The streams for the js.talks();">
+  <?php include "head.html"; ?>
+</head>
+
+<body class="streaming">
+
+  <?php include "headernav.php"; ?>
+
+  <div class="content_wrap section-streaming">
+    <section class="main section-streaming">
+      <header>
+        <h3>Event Streaming</h3>
+      </header>
+      <?php include "social.html"; ?>
+      <div class="streams-wrapper">
+        <div id="stream-track-1" class="stream-box">
+          <h2 class="stream-header">Track 1</h2>
+          <div class="embed-container">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/Ek-PsVB5FQs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <!-- <iframe width="560" height="315" src="https://www.youtube.com/embed/2Wv6OUI0Qrg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
+            <!-- <iframe width="560" height="315" src="https://www.youtube.com/embed/765jFfTtG1U" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
+          </div>
+          <div id="stream-meta-track-1" class="stream-meta">
+            <p class="stream-metadata"></p>
+          </div>
+        </div>
+      </div>
+      <div class="streams-wrapper">
+        <div id="stream-track-2" class="stream-box">
+          <h2 class="stream-header">Track 2</h2>
+          <div class="embed-container">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/kFgdkfFhsCI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+          <div id="stream-meta-track-2" class="stream-meta">
+            <p class="stream-metadata"></p>
+          </div>
+        </div>
+        <div class="stream-box">
+          <h2 id="stream-track-3" class="stream-header">Track 3</h2>
+          <div class="embed-container">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/L89ccdbZJ_8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+          <div id="stream-meta-track-3" class="stream-meta">
+            <p class="stream-metadata"></p>
+          </div>
+        </div>
+      </div>
+      <div class="streams-wrapper">
+        <div id="stream-track-4" class="stream-box">
+          <h2 class="stream-header">Track 4</h2>
+          <div class="embed-container">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/UvhnIgI2xS8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+          <div id="stream-meta-track-4" class="stream-meta">
+            <p class="stream-metadata"></p>
+          </div>
+        </div>
+        <div class="stream-box">
+          <h2 id="stream-track-5" class="stream-header">Track 5</h2>
+          <div class="embed-container">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/TWWmlrgQF44" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+          <div id="stream-meta-track-5" class="stream-meta">
+            <p class="stream-metadata"></p>
+          </div>
+        </div>
+      </div>
+      <a href="#" class="top">top</a>
+    </section>
+
+    <section class="main section-streaming">
+      <header>
+        <h3>Sponsors' rooms</h3>
+      </header>
+      <div class="sponsors-thumbs-wrapper">
+        <div class="sponsors-thumb">
+          <a href="https://us02web.zoom.us/j/86210664516" class="sponsor-thumb" target="_blank" title="Meet Mentormate">
+            <img src="/images/meeting-icons/zoom.png" height="96" width="96" />
+            Mentormate
+          </a>
+        </div>
+        <div class="sponsors-thumb">
+          <a href="https://sumup.zoom.us/j/91361685691#success" class="sponsor-thumb" target="_blank" title="Meet SumUp">
+            <img src="/images/meeting-icons/zoom.png" height="96" width="96" />
+            SumUp
+          </a>
+        </div>
+        <div class="sponsors-thumb">
+          <a href="https://starsgroup.zoom.us/j/99215996268" class="sponsor-thumb" target="_blank" title="Meet PokerStars">
+            <img src="/images/meeting-icons/zoom.png" height="96" width="96" />
+            PokerStars
+          </a>
+        </div>
+        <div class="sponsors-thumb">
+          <a href="https://meet.google.com/usv-bquo-upr" class="sponsor-thumb" target="_blank" title="Meet Anakatech">
+            <img src="/images/meeting-icons/googlemeet.png" height="96" width="96" />
+            Anakatech
+          </a>
+        </div>
+        <div class="sponsors-thumb">
+          <a href="https://hopin.com/events/isobar_jstalks" class="sponsor-thumb" target="_blank" title="Meet Isobar">
+            <img src="/images/meeting-icons/microsoft-teams.png" height="96" width="96" />
+            Isobar
+          </a>
+        </div>
+        <div class="sponsors-thumb">
+          <a href="https://epa.ms/VRoom" class="sponsor-thumb" target="_blank" title="Meet EPAM">
+            <img src="/images/meeting-icons/microsoft-teams.png" height="96" width="96" />
+            EPAM
+          </a>
+        </div>
+        <div class="sponsors-thumb">
+          <a href="https://devexperts.zoom.us/j/9888788484" class="sponsor-thumb" target="_blank" title="Meet DevExperts">
+            <img src="/images/meeting-icons/zoom.png" height="96" width="96" />
+            DevExperts
+          </a>
+        </div>
+        <div class="sponsors-thumb">
+          <a href="https://meet.google.com/ppo-hgdk-qmn" class="sponsor-thumb" target="_blank" title="Meet Chaos Group">
+            <img src="/images/meeting-icons/googlemeet.png" height="96" width="96" />
+            Chaos Group
+          </a>
+        </div>
+        <div class="sponsors-thumb">
+          <a href="https://draftkings.zoom.us/j/4581110206?pwd=L0tQc0tZTzVQSi9GUDcxazhtemd6dz09" class="sponsor-thumb" target="_blank" title="Meet DraftKings">
+            <img src="/images/meeting-icons/zoom.png" height="96" width="96" />
+            DraftKings
+          </a>
+        </div>
+        <div class="sponsors-thumb">
+          <a href="https://meet.google.com/rmg-jqbw-pnb" class="sponsor-thumb" target="_blank" title="Meet Via">
+            <img src="/images/meeting-icons/googlemeet.png" height="96" width="96" />
+            Via
+          </a>
+        </div>
+        <div class="sponsors-thumb">
+          <a href="https://teams.microsoft.com/l/meetup-join/19%3ameeting_ZDdlMGFlNzUtMDgyOC00YTk0LWE4MjgtNmI4Yzg1YzQ4M2Mw%40thread.v2/0?context=%7b%22Tid%22%3a%22cb0e348e-beb0-46b4-b0e8-de3f281e0035%22%2c%22Oid%22%3a%22be529a2b-8374-4156-b0aa-6c838a2754a2%22%7d" class="sponsor-thumb" target="_blank" title="Meet Strypes">
+            <img src="/images/meeting-icons/microsoft-teams.png" height="96" width="96" />
+            Strypes
+          </a>
+        </div>
+      </div>
+      <a href="#" class="top">top</a>
+    </section>
+
+    <?php include "sponsors.html"; ?>
+
+  </div>
+  <?php include "footer.html"; ?>
+  <script>
+    var fetchedData = {
+      rooms: [],
+      sessions: [],
+      speakers: [],
+      questions: [],
+      categories: [],
+    };
+    var refreshTimeout = -1;
+    /*
+      { // SESSION:
+        categoryItems: string[]
+        description: string
+        startsAt: DATE ISO
+        endsAt: DATE ISO
+        id: string
+        isPlenumSession: boolean
+        isServiceSession: boolean
+        questionAnswers: string[]
+        roomId: number
+        speakers: string[]
+        title: string
+      }
+
+      { // SPEAKER
+        bio: string
+        categoryItems: string[]
+        firstName: string
+        fullName: string
+        id: string
+        isTopSpeaker: boolean
+        lastName: string
+        links: []
+        profilePicture: string
+        questionAnswers: string[]
+        sessions: number[]
+        tagLine: string
+      }
+
+      { // ROOM
+        "id": number
+        "name": string
+        "sort": number
+      }
+    */
+
+    function getSessionByTimeAndTrack(sessions, time) {
+      return sessions
+        .sort((a, b) => new Date(a.startsAt).getTime() - new Date(b.startsAt).getTime())
+        .find(x => new Date(x.startsAt).getTime() < time.getTime() && time.getTime() < new Date(x.endsAt).getTime());
+    }
+
+    function getSpeakersForSession(speakers, session) {
+      return speakers.filter(x => session.speakers.indexOf(x.id) > -1);
+    }
+
+    function metaTemplate(session, speakers) {
+      if (!session) {
+        return "";
+      }
+
+      const talk = `talking about <strong>${session.title}</strong>`;
+      const names = speakers.map(x => x.fullName);
+
+      let out = `Now playing: <strong>${session.title}</strong>`;
+
+      if (speakers.length > 1) {
+        out = `<strong>${names.join(", ")}</strong> are ${talk}`;
+      }
+
+      if (speakers.length === 1) {
+        out = `<strong>${names.join("")}</strong> is ${talk}`;
+      }
+
+      return `<p class="stream-metadata">${out}</p>`;
+    }
+
+    function trackMeta(sessions, speakers, time) {
+      const currentSession = getSessionByTimeAndTrack(sessions, time);
+
+      if (!currentSession) {
+        return [
+          null,
+          []
+        ]
+      }
+
+      const currentSpeakers = getSpeakersForSession(speakers, currentSession);
+
+      if (!currentSpeakers) {
+        return [
+          currentSession, []
+        ]
+      }
+
+      return [currentSession, currentSpeakers]
+    }
+
+    function recurringUpdate() {
+      clearTimeout(refreshTimeout);
+      main(fetchedData);
+      refreshTimeout = setTimeout(recurringUpdate, 1000 * 60);
+    }
+
+    function updateTrackTitle(room, i) {
+      const header = document.querySelector(`#stream-track-${i+1} .stream-header`);
+      if (header) {
+        header.innerHTML = room.name
+      }
+    }
+
+    function main(data) {
+      if (data.rooms) {
+        const rooms = data.rooms.sort((a, b) => a.sort - b.sort);
+        rooms.forEach(updateTrackTitle);
+
+        if (data.sessions && data.speakers) {
+          const now = new Date();
+          rooms
+            .map(x => {
+              return metaTemplate(
+                ...trackMeta(
+                  data.sessions.filter(y => y.roomId === x.id),
+                  data.speakers,
+                  now
+                ))
+            })
+            .forEach((x, i) => {
+              const div = document.getElementById(`stream-meta-track-${i + 1}`);
+              if (div) {
+                const p = div.querySelector(".stream-metadata");
+                if (p && p.outerHTML !== x) {
+                  div.removeChild(p);
+                  div.insertAdjacentHTML("beforeend", x);
+                }
+              }
+            })
+        }
+      }
+    }
+
+    function fetchData() {
+      return fetch("https://sessionize.com/api/v2/g92oqtdh/view/All")
+        .then(x => {
+          if (x.ok) {
+            return x.json();
+          }
+          throw new Error();
+        })
+        .catch(e => {
+          console.error(e);
+          return {
+            rooms: [],
+            sessions: [],
+            speakers: [],
+            questions: [],
+            categories: [],
+          };
+        })
+    }
+
+    window.addEventListener("load", function() {
+      setTimeout(function() {
+        fetchData()
+          .then(x => {
+            fetchedData = x;
+            recurringUpdate();
+          })
+      }, 0);
+    });
+  </script>
+</body>
+
+</html>
